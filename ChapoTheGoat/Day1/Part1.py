7238fac8f56d983ea61fa76d987e9a43b8eb6bf7
ChapoTheGoat <gama_giovanni@yahoo.com>
2023-12-09 10:40:28 -0600

    Day 1 Par 1 & 2 (Progress)
    Too EZ

--- ChapoTheGoat/Day1/Part1.py
+++ ChapoTheGoat/Day1/Part1.py
@@ -1,1 +1,12 @@
-print('Hello World')
+import re
+
+with open('input.txt', 'r') as file:
+    dad = 0
+    for line in file:
+        digit_pattern = re.compile(r'\d')
+        digits = digit_pattern.findall(line)
+        first_dig = digits[0]
+        last_dig = digits[-1]
+        cal_val = int(first_dig + last_dig)
+        dad += cal_val
+print(dad)
