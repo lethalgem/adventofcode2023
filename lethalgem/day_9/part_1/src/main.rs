fea37ec2a24dcd7e1e00410a5f978d3723f1b1fa
Ian Cash <iwc123@gmail.com>
2023-12-12 09:40:44 -0600

    Mostly finish day 9 pt 1

---
+++ lethalgem/day_9/part_1/src/main.rs
@@ -0,0 +1,120 @@
+use std::{fs, io, num::ParseIntError};
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum Day8Error {
+    #[error("File not loaded")]
+    UnableToLoadFile(#[from] io::Error),
+    #[error("Failed to parse int")]
+    ParseIntFailed(#[from] ParseIntError),
+    #[error("Failed to find last value in sequence {0:?}")]
+    NoLastValueInSequence(Vec<i32>),
+}
+
+fn main() {
+    if let Err(err) = run() {
+        eprintln!("Error: {}", err)
+    }
+}
+
+fn run() -> Result<(), Day8Error> {
+    let start = std::time::Instant::now();
+
+    let input_data = load_input("src/input.txt".to_string())?;
+
+    // println!("parsing hands, time elapsed: {:?}", start.elapsed());
+    // let mut hands = extract_hands(input_data.to_owned())?;
+
+    Ok(())
+}
+
+fn load_input(file_path: String) -> Result<String, Day8Error> {
+    let data = fs::read_to_string(file_path).map_err(Day8Error::UnableToLoadFile)?;
+    println!("Successfully loaded file");
+    Ok(data)
+}
+
+fn extrapolate_all_values(input: ) // TODO: extrapolate and sum
+
+fn predict_next_reading(sequence: Vec<i32>) -> Result<i32, Day8Error> {
+    let mut found_zero_sequence = false;
+    let mut final_steps: Vec<i32> = vec![*sequence
+        .last()
+        .ok_or_else(|| Day8Error::NoLastValueInSequence(sequence.clone()))?];
+    let mut next_sequence: Vec<i32> = sequence;
+
+    while !found_zero_sequence {
+        let sequence_evaluation = find_next_sequence(&next_sequence);
+
+        final_steps.push(
+            *sequence_evaluation
+                .0
+                .last()
+                .ok_or_else(|| Day8Error::NoLastValueInSequence(sequence_evaluation.0.clone()))?,
+        );
+        found_zero_sequence = sequence_evaluation.1;
+
+        next_sequence = sequence_evaluation.0;
+    }
+
+    Ok(final_steps.iter().sum())
+}
+
+// return sequence of steps, and if sequence was all 0's
+fn find_next_sequence(sequence: &Vec<i32>) -> (Vec<i32>, bool) {
+    let mut is_zero_sequence = true;
+    let mut next_sequence: Vec<i32> = Vec::new();
+    for (i, _) in sequence.iter().enumerate() {
+        if i > 0 {
+            let step = sequence[i] - sequence[i - 1];
+            next_sequence.push(step);
+
+            if step != 0 {
+                is_zero_sequence = false;
+            }
+        }
+    }
+
+    (next_sequence, is_zero_sequence)
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{find_next_sequence, predict_next_reading};
+
+    fn check(actual: &str, expect: expect_test::Expect) {
+        expect.assert_eq(actual);
+    }
+
+    #[test]
+    fn find_correct_steps() {
+        let input = [0, 3, 6, 9, 12, 15];
+        let result = find_next_sequence(&input.to_vec());
+        check(
+            &format!("{:?}", result),
+            expect_test::expect!["([3, 3, 3, 3, 3], false)"],
+        );
+
+        let input = [3, 3, 3, 3, 3];
+        let result = find_next_sequence(&input.to_vec());
+        check(
+            &format!("{:?}", result),
+            expect_test::expect!["([0, 0, 0, 0], true)"],
+        );
+    }
+
+    #[test]
+    fn find_correct_next_reading() {
+        let input = [0, 3, 6, 9, 12, 15];
+        let result = predict_next_reading(input.to_vec()).unwrap();
+        check(&format!("{:?}", result), expect_test::expect!["18"]);
+
+        let input = [1, 3, 6, 10, 15, 21];
+        let result = predict_next_reading(input.to_vec()).unwrap();
+        check(&format!("{:?}", result), expect_test::expect!["28"]);
+
+        let input = [10, 13, 16, 21, 30, 45];
+        let result = predict_next_reading(input.to_vec()).unwrap();
+        check(&format!("{:?}", result), expect_test::expect!["68"]);
+    }
+}
