7f66cf631dce51024aa599d5bc537690c6468c59
Ian Cash <iwc123@gmail.com>
2023-12-13 11:38:53 -0600

    Fix folder structure

--- lethalgem/day_9/part_2/src/main.rs
+++ lethalgem/day_9/part_2/src/main.rs
@@ -45,26 +45,26 @@ fn extrapolate_all_values(input: String) -> Result<i32, Day8Error> {
             .map(|num| num.parse::<i32>())
             .collect::<Result<Vec<i32>, _>>()?;
 
-        sum += predict_next_reading(sequence)?;
+        sum += predict_first_reading(sequence)?;
     }
 
     Ok(sum)
 }
 
-fn predict_next_reading(sequence: Vec<i32>) -> Result<i32, Day8Error> {
+fn predict_first_reading(sequence: Vec<i32>) -> Result<i32, Day8Error> {
     let mut found_zero_sequence = false;
-    let mut final_steps: Vec<i32> = vec![*sequence
-        .last()
+    let mut first_steps: Vec<i32> = vec![*sequence
+        .first()
         .ok_or_else(|| Day8Error::NoLastValueInSequence(sequence.clone()))?];
     let mut next_sequence: Vec<i32> = sequence;
 
     while !found_zero_sequence {
         let sequence_evaluation = find_next_sequence(&next_sequence);
 
-        final_steps.push(
+        first_steps.push(
             *sequence_evaluation
                 .0
-                .last()
+                .first()
                 .ok_or_else(|| Day8Error::NoLastValueInSequence(sequence_evaluation.0.clone()))?,
         );
         found_zero_sequence = sequence_evaluation.1;
@@ -72,11 +72,23 @@ fn predict_next_reading(sequence: Vec<i32>) -> Result<i32, Day8Error> {
         next_sequence = sequence_evaluation.0;
     }
 
-    Ok(final_steps.iter().sum())
+    first_steps.reverse();
+    let mut predicted_first_step = *first_steps
+        .first()
+        .ok_or_else(|| Day8Error::NoLastValueInSequence(first_steps.clone()))?;
+    for (i, _) in first_steps.iter().enumerate() {
+        if i + 1 == first_steps.len() {
+            break;
+        };
+        let new_predicted_step = first_steps[i + 1] - predicted_first_step;
+        predicted_first_step = new_predicted_step
+    }
+
+    Ok(predicted_first_step)
 }
 
 // return sequence of steps, and if sequence was all 0's
-fn find_next_sequence(sequence: &Vec<i32>) -> (Vec<i32>, bool) {
+fn find_next_sequence(sequence: &[i32]) -> (Vec<i32>, bool) {
     let mut is_zero_sequence = true;
     let mut next_sequence: Vec<i32> = Vec::new();
     for (i, _) in sequence.iter().enumerate() {
@@ -95,7 +107,7 @@ fn find_next_sequence(sequence: &Vec<i32>) -> (Vec<i32>, bool) {
 
 #[cfg(test)]
 mod tests {
-    use crate::{extrapolate_all_values, find_next_sequence, load_input, predict_next_reading};
+    use crate::{extrapolate_all_values, find_next_sequence, load_input, predict_first_reading};
 
     fn check(actual: &str, expect: expect_test::Expect) {
         expect.assert_eq(actual);
@@ -104,14 +116,14 @@ mod tests {
     #[test]
     fn find_correct_steps() {
         let input = [0, 3, 6, 9, 12, 15];
-        let result = find_next_sequence(&input.to_vec());
+        let result = find_next_sequence(input.as_ref());
         check(
             &format!("{:?}", result),
             expect_test::expect!["([3, 3, 3, 3, 3], false)"],
         );
 
         let input = [3, 3, 3, 3, 3];
-        let result = find_next_sequence(&input.to_vec());
+        let result = find_next_sequence(input.as_ref());
         check(
             &format!("{:?}", result),
             expect_test::expect!["([0, 0, 0, 0], true)"],
@@ -121,22 +133,22 @@ mod tests {
     #[test]
     fn find_correct_next_reading() {
         let input = [0, 3, 6, 9, 12, 15];
-        let result = predict_next_reading(input.to_vec()).unwrap();
-        check(&format!("{:?}", result), expect_test::expect!["18"]);
+        let result = predict_first_reading(input.to_vec()).unwrap();
+        check(&format!("{:?}", result), expect_test::expect!["-3"]);
 
         let input = [1, 3, 6, 10, 15, 21];
-        let result = predict_next_reading(input.to_vec()).unwrap();
-        check(&format!("{:?}", result), expect_test::expect!["28"]);
+        let result = predict_first_reading(input.to_vec()).unwrap();
+        check(&format!("{:?}", result), expect_test::expect!["0"]);
 
         let input = [10, 13, 16, 21, 30, 45];
-        let result = predict_next_reading(input.to_vec()).unwrap();
-        check(&format!("{:?}", result), expect_test::expect!["68"]);
+        let result = predict_first_reading(input.to_vec()).unwrap();
+        check(&format!("{:?}", result), expect_test::expect!["5"]);
     }
 
     #[test]
     fn find_correct_sum() {
         let input = load_input("src/example_1.txt".to_owned()).unwrap();
         let result = extrapolate_all_values(input).unwrap();
-        check(&format!("{:?}", result), expect_test::expect!["114"]);
+        check(&format!("{:?}", result), expect_test::expect!["2"]);
     }
 }
